7932fc3ccfedf4a1e8dd53acee7d75760c1a69d3
KValexander <ewoase@gmail.com>
2022-09-26 22:35:19 +0300

    upd

--- b_hello_cargo/target/debug/deps/b_hello_cargo-8aefdfb29a16f634.d
+++
@@ -1,5 +0,0 @@
-C:\server\git\rust-training\b_hello_cargo\target\debug\deps\b_hello_cargo-8aefdfb29a16f634.rmeta: src\main.rs
-
-C:\server\git\rust-training\b_hello_cargo\target\debug\deps\b_hello_cargo-8aefdfb29a16f634.d: src\main.rs
-
-src\main.rs:
--- c_guess_game/target/debug/deps/c_guess_game.d
+++
@@ -1,5 +0,0 @@
-C:\server\git\rust-training\c_guess_game\target\debug\deps\c_guess_game.exe: src\main.rs
-
-C:\server\git\rust-training\c_guess_game\target\debug\deps\c_guess_game.d: src\main.rs
-
-src\main.rs:
