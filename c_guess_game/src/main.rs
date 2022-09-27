19858f07054ef54f3d601c2f073cd20d854a25b2
KValexander <ewoase@gmail.com>
2022-09-27 21:46:37 +0300

    upd

--- c_guess_game/src/main.rs
+++ c_guess_game/src/main.rs
@@ -34,7 +34,10 @@ fn main() {
             .expect("Failed to read line");
 
         /* Check input value */
-        let guess : u32 = guess.trim().parse().expect("Please type a number!");
+        let guess : u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
 
         /* Output */
         println!("You guessed: {guess} ");
